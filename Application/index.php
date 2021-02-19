861ebac6477937bb2699b58b1b934998fe666d3e
Mouhcine <mouhcine.mesmouki7@gmail.com>
2021-02-19 03:05:15 +0100

    deleting omar from footer

--- Application/index.php
+++ Application/index.php
@@ -80,7 +80,7 @@
       </div>
       <div class="footer-copyright">
          <div class="container">
-            Made by <a class="brown-text text-lighten-3" href="http://github.com/crushoverride007" target="blank">Mouhcine MESMOUKI and Omar ABDELMOUJIB</a>
+            Made by <a class="brown-text text-lighten-3" href="http://github.com/crushoverride007" target="blank">Mouhcine MESMOUKI and </a>
          </div>
       </div>
    </footer>
